6e7afd29e66d065c64de5c8d78faf0fbdb5c2e04
Juho <juho.ylisir@gmail.com>
2023-11-29 14:45:28 +0200

    29.11.2023

--- module_1/m1_t4/m1_t4.js
+++ module_1/m1_t4/m1_t4.js
@@ -2,5 +2,20 @@ let Gryffindor = 1
 let Slytherin = 2
 let Hufflepuff = 3
 let Ravenclaw = 4
-
+let num = Math.floor(Math.random() * 5)
 let name = prompt("Enter your name: ")
+
+switch (num) {
+    case Gryffindor:
+         document.querySelector('#text1').innerHTML = `Congratulations ${name}, you are in Gryffindor.`
+        break
+    case Slytherin:
+        document.querySelector('#text1').innerHTML = `Congratulations ${name}, you are in Slytherin.`
+        break
+    case Hufflepuff:
+        document.querySelector('#text1').innerHTML = `Congratulations ${name}, you are in Hufflepuff.`
+        break
+    case Ravenclaw:
+        document.querySelector('#text1').innerHTML = `Congratulations ${name}, you are in Ravenclaw.`
+        break
+}
